e9a8db220eb3499ad4a9de3780abbd4049adfe11
Thom-Mundstock <thomasmundstock@gmail.com>
2022-02-16 14:59:36 -0300

    Add Product Model

--- GBCSportingWeb/Models/Product.cs
+++ GBCSportingWeb/Models/Product.cs
@@ -1,6 +1,19 @@
-﻿namespace GBCSportingWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GBCSportingWeb.Models
 {
     public class Product
     {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Please enter product name.")]
+        public string? ProductName { get; set; }
+
+        [Required(ErrorMessage = "Please enter the yearly price.")]
+        [Range(0, 9999.99, ErrorMessage = "Please enter a positive value.")]
+        public double? YearlyPrice { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
     }
 }
